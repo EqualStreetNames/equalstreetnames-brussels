3b2d41384e27243af5b306f96db088d5b649391b
Jonathan Beliën <dev@jbelien.be>
2021-05-15 18:33:16 +0200

    🗃 Update data

--- config.php
+++ config.php
@@ -54,54 +54,8 @@ return [
     ],
 
     'gender' => [
-        'relation' => [
-            '3217433'  => 'M', // Avenue Chanoine Roose - Kanunnik Rooselaan
-            '3210021'  => 'M', // Avenue Docteur Lemoine - Dokter Lemoinelaan
-            '3258838'  => 'M', // Avenue Général de Longueville - Generaal de Longuevillelaan
-            '2984376'  => 'M', // Avenue Georges Henri - Georges Henrilaan
-            '3110486'  => 'M', // Avenue Hansen-Soulie - Hansen-Soulielaan
-            '3495502'  => 'M', // Avenue Père Hilaire - Pater Hilariuslaan
-            '3500230'  => 'M', // Avenue Saint-Jean - Sint-Janslaan
-            '11087006' => 'M', // Rue Bosquet - Bosquetstraat
-            '3257052'  => 'M', // Rue Coenraets - Coenraetsstraat
-            '3253232'  => 'M', // Rue de Bruyn - de Bruynstraat
-            '3288989'  => 'M', // Rue de Fierlant - de Fierlantstraat
-            '3252684'  => 'M', // Rue de Liedekerke - de Liedekerkestraat
-            '3297585'  => 'M', // Rue de Ribaucourt - de Ribaucourtstraat
-            '2966746'  => 'M', // Rue Docteur Huet - Dokter Huetstraat
-            '3256869'  => 'M', // Rue Docteur Jacobs - Dokter Jacobsstraat
-            '3312912'  => 'M', // Rue Doyen Adriaens - Deken Adriaensstraat
-            '3311841'  => 'M', // Rue Doyen Fierens - Deken Fierensstraat
-            '3138882'  => 'M', // Rue Félix De Cuyper - Félix De Cuyperstraat
-            '3198269'  => 'M', // Rue François Gérard - François Gérardstraat
-            '3474783'  => 'M', // Rue François Janssens - François Janssensstraat
-            '3367670'  => 'M', // Rue Général Michel - Generaal Michelstraat
-            '3489867'  => 'M', // Rue Général Ruquoy - Generaal Ruquoystraat
-            '2974969'  => 'M', // Rue Gillebertus - Gillebertusstraat
-            '3302989'  => 'M', // Rue Ulens - Ulensstraat
-            '3004965'  => 'M', // Rue Wayez - Wayezstraat
-        ],
-        'way' => [
-            '14795061'  => 'M', // Avenue des Frères Goemaere - Gebroeders Goemaerelaan
-            '458810331' => 'M', // Avenue des Frères Goemaere - Gebroeders Goemaerelaan
-            '35830442'  => 'M', // Avenue des Frères Legrain - Gebroeders Legrainlaan
-            '8250579'   => 'M', // Avenue des Frères Legrain - Gebroeders Legrainlaan
-            '863287984' => 'M', // Avenue des Frères Legrain - Gebroeders Legrainlaan
-            '148190425' => 'M', // Avenue Georges Henri - Georges Henrilaan
-            '115117025' => 'M', // Impasse Madrille - Madrillegang
-            '572851813' => 'M', // Impasse Madrille - Madrillegang
-            '132433013' => 'M', // Impasse Ronsmans - Ronsmansgang
-            '132433014' => 'M', // Impasse Ronsmans - Ronsmansgang
-            '132433015' => 'M', // Impasse Ronsmans - Ronsmansgang
-            '243608882' => 'M', // Rue Beeckman - Beeckmanstraat
-            '244299516' => 'M', // Rue Beeckman - Beeckmanstraat
-            '243608671' => 'M', // Rue Beeckman - Beeckmanstraat
-            '4426075'   => 'M', // Rue Blanche - Blanchestraat
-            '13768131'  => 'M', // Rue Crickx - Crickxstraat
-            '210294010' => 'M', // Rue Georges Huygens - Georges Huygensstraat
-            '496191041' => 'M', // Rue Georges Huygens - Georges Huygensstraat
-            '151163014' => 'M', // Rue Timmermans - Timmermansstraat
-        ],
+        'relation' => [],
+        'way' => [],
     ],
 
     'instances' => [
